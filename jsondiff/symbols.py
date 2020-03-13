82c4d62f4ca202c1332c46770218a58533491919
Francisco Javier Martinez Lago <edoreld@gmail.com>
2020-03-13 21:42:14 +0100

    Update symbols.py

--- jsondiff/symbols.py
+++ jsondiff/symbols.py
@@ -8,6 +8,12 @@ class Symbol(object):
 
     def __str__(self):
         return "$" + self.label
+    
+    def __hash__(self):
+        return hash((self.label))
+
+    def __eq__(self, other):
+        return (self.label) == (other.label)
 
 missing = Symbol('missing')
 identical = Symbol('identical')
